d3c6894a76845e562a913f899a55380d517b0c76
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-05-09 18:57:40 +0800

    Initialize and Implement ML Model

---
+++ ProductReviewML/ProductReviewML/Program.cs
@@ -0,0 +1,26 @@
+﻿//Load sample data
+using ProductReviewML;
+
+var sort = new SortHelper();
+
+Console.Write("Input: ");
+var review = Console.ReadLine();
+
+
+if (review != null)
+{
+    var sampleData = new SentimentModel.ModelInput()
+    {
+        Review = review
+    };
+
+    //Load model and predict output
+    var result = SentimentModel.Predict(sampleData);
+
+    Console.WriteLine("\nResult: " + result.PredictedLabel + "\n");
+    sort.Sort(result.Score[0], result.Score[1], result.Score[2], result.Score[3]);
+}
+else
+{
+    Console.WriteLine("Error: Input is null");
+}
---
+++ ProductReviewML/ProductReviewML/SortHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductReviewML
+{
+    public class SortHelper
+    {
+        public void Sort(float positive, float negative, float neutral, float notRelated)
+        {
+            List<float> init = new List<float> { positive, negative, neutral, notRelated};
+            int i = 0;
+            init.Sort();
+            init.Reverse();
+
+            while (i < 4)
+            {
+                if (positive == init[i])
+                {
+                    Console.WriteLine($"Positive: {Math.Round(positive * 100, 0)}%");
+                }
+                else if (negative == init[i])
+                {
+                    Console.WriteLine($"Negative: {Math.Round(negative * 100, 0)}%");
+                }
+                else if (neutral == init[i])
+                {
+                    Console.WriteLine($"Neutral: {Math.Round(neutral * 100, 0)}%");
+                }
+                else if(notRelated== init[i])
+                {
+                    Console.WriteLine($"Not Related: {Math.Round(notRelated * 100, 0)}%");
+                }
+                i++;
+            }
+        }
+    }
+}
